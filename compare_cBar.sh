35078620ee6045153733d00c3c1a7a1d90226a03
philippmuench <philipp.muench@helmholtz-hzi.de>
2017-09-17 21:50:09 +0200

    update with new data

--- compare_cBar.sh
+++ compare_cBar.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+rm -rf plasmidminer/dat_tmp
+mkdir -p plasmidminer/dat_tmp
+rm -f contig_report_string.txt
+rm -f /home/data/contig_reports/*
+
 # contig level
 while read file; do
+  # run cbar
   start_time=`date +%s`
   /root/scr/cBar.1.2/cBar.pl /home/data/fasta/contig/$file /home/data/contig_reports/cbar_report_$file.txt
   end_time=`date +%s`
   echo execution time was `expr $end_time - $start_time` s.
-  cd plasmidminer
+  
+  # run plasmidminer
   cd /root/plasmidminer
   start_time=`date +%s`
-  python plasmidminer/predict.py -m /home/data/models/model_kleb.pkl -i /home/data/fasta/contig/$file --sliding
+  rm contig_report_string.txt
+  python plasmidminer/predict.py -m /home/data/models/test.pkl -i /home/data/fasta/contig/$file --sliding
   echo execution time was `expr $end_time - $start_time` s.
   cp contig_report_string.txt /home/data/contig_reports/pm_report_$file.txt
   cd /root
+
+  # run deep-plasmid
+  cd dna_lstm
+  python predict.py -i /home/data/fasta/contig/$file -w /home/data/models/weights_78.hdf5 -m /home/data/models/model.json
+  cp report_string.txt /home/data/contig_reports/deep_report_$file.txt
+  cd /root
+
   Rscript --vanilla plot.R "$file"
+
 done </home/data/file_contig_list.txt
 
 
--- plot.R
+++ plot.R
@@ -2,10 +2,11 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-#name <- args[1]
-name <- 'ref_small.fasta'
+name <- args[1]
+#name <- 'small.fasta'
 pm <- read.table(paste0('/home/data/contig_reports/pm_report_', name, '.txt'), sep=',', header=F)
 cbar <- read.table(paste0('/home/data/contig_reports/cbar_report_', name, '.txt'), sep='\t', header=F)
+deep <- read.table(paste0('/home/data/contig_reports/deep_report_', name, '.txt'), sep=',', header=F)
 
 truth_pm <- substring(pm$V1, 1,3)
 pm_pla_ind <- which(truth_pm == 'pos')
@@ -14,6 +15,10 @@ pm_chr_ind <- which(truth_pm == 'neg')
 truth_cbar <- substring(cbar$V1, 1,3)
 cbar_pla_ind <- which(truth_cbar == 'pos')
 cbar_chr_ind <- which(truth_cbar == 'neg')
+
+truth_deep <- substring(deep$V1, 1,3)
+deep_pla_ind <- which(truth_deep == 'pos')
+deep_chr_ind <- which(truth_deep == 'neg')
 
 #processing pm
 pm_tp <- length(which(pm$V2[pm_pla_ind] == 'plasmid'))
@@ -27,22 +32,31 @@ cbar_fp <- length(which(cbar$V3[cbar_pla_ind] == 'Chromosome'))
 cbar_tn <- length(which(cbar$V3[cbar_chr_ind] == 'Chromosome'))
 cbar_fn <- length(which(cbar$V3[cbar_chr_ind] == 'Plasmid'))
 
+#processing deep
+deep_tp <- length(which(deep$V2[deep_pla_ind] == 'plasmid'))
+deep_fp <- length(which(deep$V2[deep_pla_ind] == 'chromosome'))
+deep_tn <- length(which(deep$V2[deep_chr_ind] == 'chromosome'))
+deep_fn <- length(which(deep$V2[deep_chr_ind] == 'plasmid'))
+
 pm_precision = pm_tp / (pm_tp + pm_fp)
 pm_recall = pm_tp / (pm_tp + pm_fn)
 
 cbar_precision = cbar_tp / (cbar_tp + cbar_fp)
 cbar_recall = cbar_tp / (cbar_tp + cbar_fn)
 
+deep_precision = deep_tp / (deep_tp + deep_fp)
+deep_recall = deep_tp / (deep_tp + deep_fn)
+
 print(paste('pm_precision', pm_precision))
 print(paste('pm_recall', pm_recall))
 print(paste('cbar_precision', cbar_precision))
 print(paste('cbar_recall', cbar_recall))
+print(paste('deep_precision', deep_precision))
+print(paste('deep_recall', deep_recall))
 
-print(df)
-
-df = data.frame(PlasmidMiner = c(pm_precision, pm_recall), cBar = c(cbar_precision, cbar_recall))
+df = data.frame(PlasmidMiner = c(pm_precision, pm_recall), cBar = c(cbar_precision, cbar_recall),  deepPlasmid = c(deep_precision, deep_recall))
 rownames(df) <- c('precision', 'recall')
 
 pdf(paste0('/home/data/recall_plot_', args[1],'.pdf'))
-barplot(as.matrix(df), col=c("darkblue","red"), legend = rownames(df)) 
+barplot(as.matrix(df), col=c("darkblue","red"), legend = rownames(df), beside=TRUE) 
 dev.off()
